746196f4ccef662f937a379a644a9f3569a08555
Vyrastas <jeffjbumgardner@gmail.com>
2013-07-16 21:54:18 -0700

    Create Variables.m

---
+++ Variables.m
@@ -0,0 +1,13 @@
+//
+//  Variables.m
+//  Papercut
+//
+//  Created by Jeff Bumgardner on 1/13/13.
+//  Copyright (c) 2013 Jeff Bumgardner. All rights reserved.
+//
+
+#import "Variables.h"
+
+@implementation Variables
+
+@end
